f0afdb4abd5ef41d2caedf38aabb3fe4001cb7f4
aoixx1022 <sakamoto.ryoma1022@gmail.com>
2025-05-10 18:36:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    print(f"受信データ: {content}") 
+    print(f"受信データ: {date}") 
     content = data.get('content', {}).get('text', '')
     print(f"受信テキスト: {content}") 
     if content.count('-') >= 3:
